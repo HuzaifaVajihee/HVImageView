fcedf1f60f8ced9a5cec05f5b7725b7257e3484e
huzaifavajihee <friends.special5152@gmail.com>
2023-12-19 17:05:09 +0530

    minor changes

--- hvimageview/src/main/java/unique/solution/hvimageview/MaskImageView.java
+++ hvimageview/src/main/java/unique/solution/hvimageview/MaskImageView.java
@@ -178,7 +178,8 @@ public class MaskImageView extends AppCompatImageView {
         Rect srcRect = centerCropImage();
         c.drawBitmap(bitmap,srcRect,rect,imagePaint);
         c.drawBitmap(maskbitmap,null,rect,maskPaint);
-        //c.restore();
+        c.save();
+        c.restore();
     }
 
     public Rect centerCropImage()
